7a4ea0f4feb6aea75d1a23d09816639a7e0ee340
Permpoon Chaowanaphunphon <38369861+mossnana@users.noreply.github.com>
2019-01-24 15:24:29 +0700

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,8 +1,16 @@
+// นำเข้า library firebase
 import * as firebase from 'firebase';
+// เรียกใช้บริการ Google Cloud Firestore
 import firestore from 'firebase/firestore'
-
+/*
+  ใน Google Cloud Firestore จะมี settings ต่าง ๆ สามารถไปดูได้ที่ 
+  https://firebase.google.com/docs/reference/js/firebase.firestore.Settings
+  
+  ช่วยในการป้องกันการ เทคนิคการตัดคำ (Truncation) เป็นเทคนิคที่ช่วยในการสืบค้นให้ได้ข้อมูลที่กว้างขึ้น 
+  ทำให้ Query ข้อมูลได้ดีขึ้น
+*/
 const settings = {timestampsInSnapshots: true};
-
+// ค่า config เอาได้จากใน Google Firesabe Console
 const config = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
@@ -11,8 +19,9 @@ const config = {
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_ID"
 };
+// ทำการเชื่อมต่อ Google Firebase ด้วยฟังก์ชั่น initializeApp()
 firebase.initializeApp(config);
-
+// ตั้งค่า Google Cloud Firestore
 firebase.firestore().settings(settings);
 
 export default firebase;
