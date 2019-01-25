32d90c1653d12876febdeba6007e751937e0ba4e
Permpoon Chaowanaphunphon <38369861+mossnana@users.noreply.github.com>
2019-01-25 12:54:17 +0700

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyDa8VH-8SZEDQtFssbuawhjjEYKWDGNMaU",
+  authDomain: "testmyapp-xxxxx.firebaseapp.com",
+  databaseURL: "https://testmyapp-xxxxx.firebaseio.com",
+  projectId: "testmyapp-xxxxx",
+  storageBucket: "testmyapp-xxxxx.appspot.com",
+  messagingSenderId: "900345852931"
+};
+
+export default config;
